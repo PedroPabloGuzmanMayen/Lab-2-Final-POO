5a887dc7cf861ba9bdb391455c95e159d6544571
Pedro Pablo Guzmán Mayen <guz22111@uvg.edu.gt>
2022-09-28 20:40:47 -0600

    Logica
    Se comenzaron a realizar métodos que sirven para controlar el programa

---
+++ MethodsLab2.java
@@ -0,0 +1,13 @@
+
+public class MethodsLab2 {
+	
+	public boolean canBeAdded(int programSpace, int AviableSpace) {
+		if (programSpace > AviableSpace) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+
+}
